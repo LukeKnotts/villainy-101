4b809fc5f31525a1215c4a7e8cb3a6c9ed79667f
Dragon7261 <mikayel.walker@gmail.com>
2025-03-12 11:00:21 -0600

    Grid System works

--- new-game-project/Scenes/Contuctiongrid.gd
+++ new-game-project/Scenes/Contuctiongrid.gd
@@ -1,14 +1,17 @@
 extends TileMapLayer
-
-var GridSize = 8
-var Dic = {}
-
+#needs Gridsize to work, set it to 12 beucse that is the work area
+#All of this works only in Contruction code grid
+#Conruction Area does not have code and just shows were the player can place stuff down
+var GridSize = 12
+#Set tile and code will be decribed in documentation, pls read.
 func _input(event):
+	#Left click adds a block
 	if Input.is_action_just_pressed("Leftclick"):
 		var tile = local_to_map(get_global_mouse_position())	
 		for x in GridSize:
 			for y in GridSize:
 				set_cell(tile, 0, Vector2i(0,0), 0)
+	#Right click removes a block
 	if Input.is_action_just_pressed("RightClick"):
 		var tile = local_to_map(get_global_mouse_position())	
 		for x in GridSize:
