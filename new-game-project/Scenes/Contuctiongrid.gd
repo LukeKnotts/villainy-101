992fdc5cf39038904af6988fcf0c9157c7352d4a
StaralDev <160529867+StaralDev@users.noreply.github.com>
2025-03-13 09:20:50 -0600

    Prepared grid

--- new-game-project/Scenes/Contuctiongrid.gd
+++ new-game-project/Scenes/Contuctiongrid.gd
@@ -7,6 +7,24 @@ var GridMinimum = Vector2(6, 2)
 var GridMaximum = Vector2(17, 13)
 #Set tile and code will be decribed in documentation, pls read.
 
+var grid = []
+
+## Generates a 12x12 Grid of purly null
+func prepareGrid():
+	var emptyGrid = []
+	for i in 12:
+		emptyGrid.append([])
+		emptyGrid[0].append(null)
+		
+	for i in range(1, 12):
+		if (i != 0):
+			emptyGrid[i] = emptyGrid[0].duplicate()
+			
+	return emptyGrid
+
+func _ready() -> void:
+	grid = prepareGrid()
+
 func tileInBounds(tile: Vector2):
 	return not (
 		tile.x < GridMinimum.x 
@@ -19,10 +37,11 @@ func _input(event):
 	if Input.is_action_just_pressed("Leftclick"):
 		var tile = local_to_map(get_global_mouse_position())
 		if not (tileInBounds(tile)): return
-		set_cell(tile, 0, Vector2i(0,0), 0)
-
+		grid[tile.x][tile.y] = {}
+		print(grid)
+		
 	if Input.is_action_just_pressed("RightClick"):
 		var tile = local_to_map(get_global_mouse_position())	
 		if not (tileInBounds(tile)): return
-		erase_cell(tile)
-		
+		grid[tile.x][tile.y] = null
+		print(grid)
